3b3f54e05caa5ba80a15068624bd28ed97b2fe6c
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2022-05-20 17:31:09 +0200

    Add files via upload

---
+++ how-to-call-an-async-function-using-async-let-2/SwiftConcurrencyByExample/App.swift
@@ -0,0 +1,41 @@
+//
+// From Swift Concurrency by Example by Paul Hudson
+// https://www.hackingwithswift.com/quick-start/concurrency
+//
+// You're welcome to use this code for any purpose,
+// commercial or otherwise, with or without attribution.
+//
+
+import Foundation
+
+struct User: Decodable {
+    let id: UUID
+    let name: String
+    let age: Int
+}
+
+struct Message: Decodable, Identifiable {
+    let id: Int
+    let from: String
+    let message: String
+}
+
+func fetchFavorites(for user: User) async -> [Int] {
+    print("Fetching favorites for \(user.name)…")
+
+    do {
+        async let (favorites, _) = URLSession.shared.data(from: URL(string: "https://hws.dev/user-favorites.json")!)
+        return try await JSONDecoder().decode([Int].self, from: favorites)
+    } catch {
+        return []
+    }
+}
+
+@main
+struct App {
+    static func main() async {
+        let user = User(id: UUID(), name: "Taylor Swift", age: 26)
+        async let favorites = fetchFavorites(for: user)
+        await print("Found \(favorites.count) favorites.")
+    }
+}
