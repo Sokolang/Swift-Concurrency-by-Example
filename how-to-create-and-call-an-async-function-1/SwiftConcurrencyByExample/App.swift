13bb2f5d98a224600c9d5890879f81b79e3d93f8
Anzhelika Sokolova <63107259+Sokolang@users.noreply.github.com>
2022-05-20 16:47:37 +0200

    Add files via upload

---
+++ how-to-create-and-call-an-async-function-1/SwiftConcurrencyByExample/App.swift
@@ -0,0 +1,37 @@
+//
+// From Swift Concurrency by Example by Paul Hudson
+// https://www.hackingwithswift.com/quick-start/concurrency
+//
+// You're welcome to use this code for any purpose,
+// commercial or otherwise, with or without attribution.
+//
+
+import Foundation
+
+func fetchWeatherHistory() async -> [Double] {
+    (1...100_000).map { _ in Double.random(in: -10...30) }
+}
+
+func calculateAverageTemperature(for records: [Double]) async -> Double {
+    let total = records.reduce(0, +)
+    let average = total / Double(records.count)
+    return average
+}
+
+func upload(result: Double) async -> String {
+    "OK"
+}
+
+func processWeather() async {
+    let records = await fetchWeatherHistory()
+    let average = await calculateAverageTemperature(for: records)
+    let response = await upload(result: average)
+    print("Server response: \(response)")
+}
+
+@main
+struct App {
+    static func main() async {
+        await processWeather()
+    }
+}
